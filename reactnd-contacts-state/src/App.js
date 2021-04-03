3ccd869a617fe1a2ce9fcd1d63e9facd60cdaa75
Shahnawaz Alam <shahnawaz.alam13@gmail.com>
2021-04-03 22:00:40 +1100

    A new version contacts app with states used where the remove button updates the state to remove the contact

---
+++ reactnd-contacts-state/src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import ListContacts from './ListContacts'
+
+class App extends Component {
+  state = {
+    contacts: [
+      {
+        id: 'tyler',
+        name: 'Tyler McGinnis',
+        handle: '@tylermcginnis',
+        avatarURL: 'http://localhost:5001/tyler.jpg'
+      },
+      {
+        id: 'karen',
+        name: 'Karen Isgrigg',
+        handle: '@karen_isgrigg',
+        avatarURL: 'http://localhost:5001/karen.jpg'
+      },
+      {
+        id: 'richard',
+        name: 'Richard Kalehoff',
+        handle: '@richardkalehoff',
+        avatarURL: 'http://localhost:5001/richard.jpg'
+      },
+    ]
+  }
+
+  removeContact = (contact) => {
+    this.setState((currentState) => ({
+      contacts: currentState.contacts.filter((c) => {
+        return c.id != contact.id 
+      })
+    }))
+  }
+
+  render() {
+    return (
+      <div>
+        <ListContacts 
+          contacts={this.state.contacts} 
+          onDeleteContact={this.removeContact}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
