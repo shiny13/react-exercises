f93f8aa5632cfb82bfdebddea38a5fc108656f0f
Shahnawaz Alam <shahnawaz.alam13@gmail.com>
2021-04-06 01:29:09 +1000

    Added route so got rid of onNavigate action listener and screen state

--- reactnd-contacts-state/src/ListContacts.js
+++ reactnd-contacts-state/src/ListContacts.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 class ListContacts extends Component {
   static propTypes = {
@@ -19,7 +20,7 @@ class ListContacts extends Component {
   }
   render () {
     const { query } = this.state;
-    const { contacts, onDeleteContact, onNavigate } = this.props;
+    const { contacts, onDeleteContact } = this.props;
     const showingContacts = query === ''
       ? contacts
       : contacts.filter((c) => (
@@ -36,11 +37,10 @@ class ListContacts extends Component {
             value={query}
             onChange={(event) => this.updateQuery(event.target.value)}
           />
-          <a
-            href="#create"
-            onClick={onNavigate}
+          <Link
+            to='/create'
             className='add-contact'
-          >Add contact</a>
+          >Add contact</Link>
         </div>
 
         {showingContacts.length != contacts.length && (
