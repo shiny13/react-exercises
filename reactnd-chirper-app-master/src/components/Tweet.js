d07015cc1fac13b8db1a53979dc2c3909b78e1eb
Shahnawaz Alam <shahnawaz.alam13@gmail.com>
2021-06-20 23:56:24 +1000

    Added action dispatcher for liking tweets, changed component in tweet to handle the action.

--- reactnd-chirper-app-master/src/components/Tweet.js
+++ reactnd-chirper-app-master/src/components/Tweet.js
@@ -5,11 +5,18 @@ import {
     TiArrowBackOutline, 
     TiHeartOutline, 
     TiHeartFullOutline } from 'react-icons/ti'
+import { handleToggleTweet } from '../actions/tweets'
 
 class Tweet extends Component {
     handleLike = (e) => {
         e.preventDefault()
-        // todo: Handle like Tweet
+        const { dispatch, tweet, authedUser } = this.props 
+
+        dispatch(handleToggleTweet({
+            id: tweet.id,
+            hasLiked: tweet.hasLiked,
+            authedUser
+        }))
     }
     toParent = (e, id) => {
         e.preventDefault()
