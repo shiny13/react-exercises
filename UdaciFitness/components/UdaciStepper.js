79297936ccd0b91831ec4f73ecf47520ae61d251
Shahnawaz Alam <shahnawaz.alam13@gmail.com>
2021-07-30 00:44:45 +1000

    Added slider and stepper logic

--- UdaciFitness/components/UdaciStepper.js
+++ UdaciFitness/components/UdaciStepper.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
+import { FontAwesome, Entypo } from '@expo/vector-icons'
 
-export default class UdaciStepper extends Component {
-    render() {
+export default function UdaciStepper 
+    ({ max, unit, step, value, onIncrement, onDecrement }) {
         return(
             <View>
-                <Text>UdaciStepper</Text>
+                <View>
+                    <TouchableOpacity onPress={onDecrement}>
+                        <FontAwesome name='minus' size={30} color={'black'} />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={onIncrement}>
+                        <FontAwesome name='plus' size={30} color={'black'} />
+                    </TouchableOpacity>
+                </View>
+                <View>
+                    <Text>{value}</Text>
+                    <Text>{unit}</Text>
+                </View>
             </View>
         )
-    }
 }
